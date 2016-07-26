038d82a9c335e950d9d499920bad68dfb0d14265
qmxie <qmxie@thoughtworks.com>
2016-07-26 09:57:53 +0800

    [#0]refactory images repo

--- bin/dep_rebuild.sh
+++ bin/dep_rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # init
-DEP_IMG=${LEANSW_DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}-dep
+DEP_IMG=${LEANSW_DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}:dep
 docker pull ${DEP_IMG} && echo "Pulling dependency image successfully." || echo "Can not pull Dependency image."
 OLD_SHA=$( docker inspect -f '{{ index .Config.Labels "lean.dependency.sha" }}' ${DEP_IMG} )
 CURRENT_SHA=$( mvn dependency:list |grep -e '\[INFO\]    ' |shasum |grep -e '[a-z0-9]*' -o )
@@ -21,9 +21,9 @@ function rebuild_dep_img ()
   [[ $?=0 ]] && echo "Successfully build dep image:" ${IMAGE_BUILD} || exit 1
   docker rmi ${DEP_IMG}
   echo "Removed:" ${DEP_IMG}
-  docker tag -f ${IMAGE_BUILD} ${LEANSW_DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}-dep
-  echo "Successfully tag image: " ${LEANSW_DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}-dep
-  docker push ${LEANSW_DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}-dep
+  docker tag -f ${IMAGE_BUILD} ${LEANSW_DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}:dep
+  echo "Successfully tag image: " ${LEANSW_DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}:dep
+  docker push ${LEANSW_DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}:dep
   docker rmi ${IMAGE_BUILD}
   [[ $?=0 ]] && echo "push done." || exit 1
   cd .. && rm -rf docker_tmp
