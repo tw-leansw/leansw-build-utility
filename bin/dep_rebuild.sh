535d35c831b17b8f6c690051cbad48979b67b31e
qmxie <qmxie@thoughtworks.com>
2016-07-26 10:34:13 +0800

    [#0] refactor image repo

--- bin/dep_rebuild.sh
+++ bin/dep_rebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # init
 DEP_IMG=${LEANSW_DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}:dep
-docker pull ${DEP_IMG} && echo "Pulling dependency image successfully." || echo "Can not pull Dependency image."
+docker pull ${DEP_IMG} && echo "Pulling dependency image successfully." || echo "Can not pull Dependency image. Still going on to build new dep image."
 OLD_SHA=$( docker inspect -f '{{ index .Config.Labels "lean.dependency.sha" }}' ${DEP_IMG} )
 CURRENT_SHA=$( mvn dependency:list |grep -e '\[INFO\]    ' |shasum |grep -e '[a-z0-9]*' -o )
 echo "[Dep Image shasum]:" ${OLD_SHA}
@@ -13,19 +13,17 @@ function rebuild_dep_img ()
   mkdir docker_tmp
   echo "Copying dependency libs to target/lib ..."
   mvn dependency:copy-dependencies -DoutputDirectory=docker_tmp/target/lib
-  [[ $?=0 ]] && echo "Copy dependencies Done." || exit 1
+  [[ $?=0 ]] && echo "Copy dependencies Done." || echo "Error occurs copy dependency libs. Aborting..." && exit 1
   cd docker_tmp
   IMAGE_BUILD=${DOCKER_IMAGE_NAME}_dep_tmp
   cat /var/lib/go-agent/pipelines/pipeline-docker-images/Dockerfile.dep > Dockerfile
   docker build --build-arg currentSHA=${CURRENT_SHA} -t ${IMAGE_BUILD} .
-  [[ $?=0 ]] && echo "Successfully build dep image:" ${IMAGE_BUILD} || exit 1
-  docker rmi ${DEP_IMG}
-  echo "Removed:" ${DEP_IMG}
-  docker tag -f ${IMAGE_BUILD} ${LEANSW_DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}:dep
-  echo "Successfully tag image: " ${LEANSW_DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}:dep
+  [[ $?=0 ]] && echo "Successfully build dep image:" ${IMAGE_BUILD} || echo "Error occurs when building dep image. Aborting..." && exit 1
+  docker rmi ${DEP_IMG} && echo "Removed original dep image:" ${DEP_IMG}
+  docker tag -f ${IMAGE_BUILD} ${LEANSW_DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}:dep && echo "Successfully tag image: " ${LEANSW_DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}:dep
+  docker rmi ${IMAGE_BUILD} && echo "Removed temp build image:" ${IMAGE_BUILD}
   docker push ${LEANSW_DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}:dep
-  docker rmi ${IMAGE_BUILD}
-  [[ $?=0 ]] && echo "push done." || exit 1
+  [[ $?=0 ]] && echo "push done." || echo "Error occurs when pushing dep images. Aborting..." && exit 1
   cd .. && rm -rf docker_tmp
   exit 0
 }
