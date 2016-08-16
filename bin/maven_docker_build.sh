4b7255f8b676a66a2699ade3067f424a3ee7d8fb
YongliuLi <ylli@thoughtworks.com>
2016-08-16 16:35:25 +0800

    [#0] add maven docker build script

---
+++ bin/maven_docker_build.sh
@@ -0,0 +1,5 @@
+source "$HOME/.jenv/bin/jenv-init.sh"
+jenv use java 1.8.0_71
+mvn clean
+mvn dependency:copy-dependencies -DincludeScope=runtime -DoutputDirectory=target/lib
+mvn -U package -Pdocker  -DskipTests=true -s /etc/maven/settings.xml
