9c7fb8cc65e5c880ca815aff3324e9b9ad4c345c
YongliuLi <ylli@thoughtworks.com>
2016-08-17 10:57:34 +0800

    [#0] remove lib

--- bin/maven_docker_build.sh
+++ bin/maven_docker_build.sh
@@ -1,5 +1,6 @@
 source "$HOME/.jenv/bin/jenv-init.sh"
 jenv use java 1.8.0_71
 mvn clean
+rm -rf src/main/docker/lib
 mvn dependency:copy-dependencies -DincludeScope=runtime -DoutputDirectory=target/lib
 mvn -U package -Pdocker  -DskipTests=true -s /etc/maven/settings.xml
