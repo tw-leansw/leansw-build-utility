035b64a6020eeefd3b141692499b85c96d890da8
YongliuLi <ylli@thoughtworks.com>
2016-10-11 18:40:32 +0800

    [#0] maven docker build

--- bin/maven_docker_build.sh
+++ bin/maven_docker_build.sh
@@ -1,6 +1,4 @@
-source "$HOME/.jenv/bin/jenv-init.sh"
-jenv use java 1.8.0_71
 mvn clean
 rm -rf src/main/docker/lib
 mvn dependency:copy-dependencies -DincludeScope=runtime -DoutputDirectory=target/lib
-mvn -U package -Pdocker  -DskipTests=true -s /etc/maven/settings.xml
+mvn -U package -Pdocker  -DskipTests=true -s settings.xml
