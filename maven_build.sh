71a5d3ae160164982d45bdb5ba2ee4e7a94ce5cb
linfan <linfan-163@163.com>
2016-10-26 16:07:50 +0800

    update settings.xml file path

--- maven_build.sh
+++ maven_build.sh
@@ -1,2 +1,5 @@
-mvn clean dependency:copy-dependencies -DincludeScope=runtime -s settings-1.0.xml -DoutputDirectory=target/lib 
-mvn -U package -DskipTests=true -s settings-1.0.xml
+#!/usr/bin/env bash
+BUILD_UTILITY_FOLDER=${1:-_}
+
+mvn clean dependency:copy-dependencies -DincludeScope=runtime -s ${BUILD_UTILITY_FOLDER}/mvn_settings.xml -DoutputDirectory=target/lib 
+mvn -U package -DskipTests=true -s ${BUILD_UTILITY_FOLDER}/mvn_settings.xml
