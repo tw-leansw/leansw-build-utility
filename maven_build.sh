f4a3f2280f6f0f4a0fa77036d148929aa6d3dfc5
twconsultants <twconsultants@users.noreply.github.com>
2016-11-29 16:38:48 +0800

    Update maven_build.sh

--- maven_build.sh
+++ maven_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 BUILD_UTILITY_FOLDER=${1:-_}
 
-mvn clean dependency:copy-dependencies -DincludeScope=runtime -s ${BUILD_UTILITY_FOLDER}/mvn_settings.xml -DoutputDirectory=target/lib 
+mvn clean dependency:copy-dependencies -U -DincludeScope=runtime -s ${BUILD_UTILITY_FOLDER}/mvn_settings.xml -DoutputDirectory=target/lib 
 mvn -U package -DskipTests=true -s ${BUILD_UTILITY_FOLDER}/mvn_settings.xml
